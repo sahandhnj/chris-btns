9f0de87daa2f56f41f0061c73e296817ff008ff4
Sahand <sahand.hnj@gmail.com>
2018-12-31 15:30:04 +0100

    init

--- src/App.js
+++ src/App.js
@@ -1,28 +1,50 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React from 'react';
+import PropTypes from 'prop-types';
+import { withStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+const styles = theme => ({
+  button: {
+    margin: theme.spacing.unit,
+  },
+  input: {
+    display: 'none',
+  },
+});
+
+function playme(code) {
+  var audio = document.getElementById('audio');
+  // audio.src = `audio${code}.mp3`;
+  audio.src= 'error.wav'
+  audio.load();
+  audio.play();
 }
 
-export default App;
+function ContainedButtons(props) {
+  const { classes } = props;
+  return (
+    <div className='container'>
+      <Button variant="contained" color="default" className={classes.button} onClick={() => { playme(1) }}>
+        1
+      </Button>
+      <Button variant="contained" color="primary" className={classes.button} onClick={() => { playme(2) }}>
+        2
+      </Button>
+      <Button variant="contained" color="secondary" className={classes.button} onClick={() => { playme(3) }}>
+        3
+      </Button>
+      <div>
+					<audio id="audio">
+						<source type="audio/mp3" />
+					</audio>
+			</div>
+    </div >
+  );
+}
+
+ContainedButtons.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(ContainedButtons);
